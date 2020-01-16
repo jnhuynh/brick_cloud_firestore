f3f4ea9c05a1832b6799327e7bd36ebc9e81e421
John Huynh <john@huynh.io>
2020-01-16 04:34:45 -0800

    Initial commit

---
+++ brick_cloud_firestore/lib/brick_cloud_firestore.dart
@@ -0,0 +1,7 @@
+library brick_cloud_firestore;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ brick_cloud_firestore_abstract/lib/brick_cloud_firestore_abstract.dart
@@ -0,0 +1,7 @@
+library brick_cloud_firestore_abstract;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
