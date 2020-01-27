fc5da827a28d5fb7ea0411287105fdfab4882fb5
John Huynh <john@huynh.io>
2020-01-27 05:46:32 -0800

    Remove unneeded class level annotation finder and rename private fields annotation finder for clarity

--- brick_cloud_firestore_build/lib/src/cloud_firestore_fields.dart
+++ brick_cloud_firestore_build/lib/src/cloud_firestore_fields.dart
@@ -4,8 +4,8 @@ import 'package:brick_build/src/annotation_finder.dart';
 import 'package:brick_build/src/utils/fields_for_class.dart';
 
 /// Convert `@CloudFirestoreField` annotations into digestible code
-class _CloudFirestoreFieldFinder extends AnnotationFinder<CloudFirestoreField> {
-  _CloudFirestoreFieldFinder();
+class _CloudFirestoreFieldAnnotationFinder extends AnnotationFinder<CloudFirestoreField> {
+  _CloudFirestoreFieldAnnotationFinder();
 
   @override
   CloudFirestoreField from(element) {
@@ -17,7 +17,7 @@ class _CloudFirestoreFieldFinder extends AnnotationFinder<CloudFirestoreField> {
 /// Discover all fields with `@CloudFirestoreField`
 class CloudFirestoreFields extends FieldsForClass<CloudFirestoreField> {
   @override
-  final finder = _CloudFirestoreFieldFinder();
+  final finder = _CloudFirestoreFieldAnnotationFinder();
 
   CloudFirestoreFields(ClassElement element) : super(element: element);
 }
