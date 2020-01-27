9d9d250a53979a75e5bc5e325154edaa99d956ea
John Huynh <john@huynh.io>
2020-01-27 05:44:34 -0800

    Add logic for building the adapter generators for cloud fire store serializable

--- brick_cloud_firestore_build/lib/src/cloud_firestore_serializable_generator.dart
+++ brick_cloud_firestore_build/lib/src/cloud_firestore_serializable_generator.dart
@@ -1,72 +1,9 @@
-// import 'package:analyzer/dart/element/element.dart';
-// // import 'package:brick_build/src/adapter_generator.dart';
-// // import 'package:brick_build/src/annotation_super_generator.dart';
-// // TODO: add tutorial
-// import 'package:brick_build/generators.dart';
-// import 'package:brick_cloud_firestore/src/cloud_firestore_field_serdes_generator.dart';
-// import 'package:brick_cloud_firestore_abstract/annotations.dart';
-// import 'package:build/build.dart';
-// import 'package:source_gen/source_gen.dart';
-
-// /// TODO: add cloud_fire_store type checker for top level annotation
-// /// Adapter generator
-// /// Output serializing code for all models with the @[ConnectCloudFirestoreSerializableWithRest] annotation
-// class CloudFirestoreSerializableGenerator
-//     extends AnnotationSuperGenerator<CloudFirestoreSerializable> {
-//   /// The prefix to the adapter name; useful if extending `CloudFirestoreSerializableRepository`.
-//   /// Defaults to `CloudFirestoreSerializable`.
-//   final String superAdapterName;
-
-//   /// The prefix to the repository name, specified when declaring the repository type in
-//   /// serializing functions; useful if extending `CloudFirestoreSerializableRepository`.
-//   /// Defaults to `CloudFirestoreSerializable`.
-//   final String repositoryName;
-
-//   /// These are prefix's for the generated adpter
-//   const CloudFirestoreSerializableGenerator({
-//     this.superAdapterName = 'CloudFirestoreSerializable',
-//     // Don't add this because this is just a provider
-//     // This property hints at the repostory
-//     // this.repositoryName = 'CloudFirestoreSerializable',
-//   });
-
-//   /// Given an [element] and an [annotation], scaffold generators
-//   List<SerdesGenerator> buildGenerators(Element element, ConstantReader annotation) {
-//     final cloud =
-//         CloudFirestoreFieldSerdesGenerator(element, annotation, repositoryName: repositoryName);
-//     final generators = <SerdesGenerator>[];
-//     generators.addAll(cloud.generators);
-//     return generators;
-//   }
-
-//   @override
-//   String generateForAnnotatedElement(element, annotation, buildStep) {
-//     final generators = buildGenerators(element, annotation);
-
-//     return generators.fold<List<String>>(<String>[], (acc, generator) {
-//       acc.add(generator.generate());
-//       return acc;
-//     }).join('\n');
-//   }
-
-//   @override
-//   String generateAdapter(Element element, ConstantReader annotation, BuildStep buildStep) {
-//     final generators = buildGenerators(element, annotation);
-
-//     final adapterGenerator = AdapterGenerator(
-//       superAdapterName: superAdapterName,
-//       className: element.name,
-//       generators: generators,
-//     );
-
-//     return adapterGenerator.generate();
-//   }
-// }
-
 import 'package:analyzer/dart/element/element.dart';
 import 'package:brick_build/src/annotation_super_generator.dart';
 import 'package:brick_cloud_firestore_abstract/annotations.dart';
 import 'package:brick_build/src/serdes_generator.dart';
+import 'package:brick_cloud_firestore_build/src/cloud_firestore_field_serializer_generator.dart';
+import 'package:brick_cloud_firestore_build/src/cloud_firestore_fields.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Output serializing code for all models with the @[CloudFirestoreSerializable] annotation
@@ -82,13 +19,22 @@ class CloudFirestoreSerializableGenerator
 
   /// Given an [element] and an [annotation], scaffold generators
   List<SerdesGenerator> buildGenerators(Element element, ConstantReader annotation) {
-    final cloudFirestoreField = CloudFirestoreFieldGenerator(
+    final fields = CloudFirestoreFields(element);
+
+    final deserializer = CloudFirestoreFieldSerializerGenerator(
       element,
-      annotation,
+      fields,
       repositoryName: repositoryName,
     );
-    final generators = <SerdesGenerator>[];
-    generators.addAll(cloudFirestoreField.generators);
-    return generators;
+    final serializer = CloudFirestoreFieldSerializerGenerator(
+      element,
+      fields,
+      repositoryName: repositoryName,
+    );
+
+    return [
+      deserializer,
+      serializer,
+    ];
   }
 }
