dfe9b3faceb69d6272bbe7203e78b42c2a48e0ff
John Huynh <john@huynh.io>
2020-01-28 04:56:42 -0800

    Remove unneeded aggregate builder

--- brick_cloud_firestore_build/lib/build_cloud_firestore.dart
+++ brick_cloud_firestore_build/lib/build_cloud_firestore.dart
@@ -6,13 +6,6 @@ import 'package:build/build.dart';
 
 final cloudFirestoreSerializableGenerator = const CloudFirestoreSerializableGenerator();
 
-Builder cloudFirestoreAggregateBuilder(options) {
-  return AggregateBuilder(requiredImports: [
-    "import 'package:brick_cloud_firestore_abstract/annotations.dart';",
-    "import 'package:brick_cloud_firestore/cloud_firestore.dart';",
-  ]);
-}
-
 Builder cloudFirestoreAdaptersBuilder(options) {
   return AdapterBuilder<CloudFirestoreSerializable>(cloudFirestoreSerializableGenerator);
 }
