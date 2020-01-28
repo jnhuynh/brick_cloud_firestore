27e3d414a9e7881e1fab04cdeb4ca60bf4161b50
John Huynh <john@huynh.io>
2020-01-28 04:40:09 -0800

    Export cloud firestore model and annotations from brick_cloud_firestore package

--- brick_cloud_firestore/lib/cloud_firestore.dart
+++ brick_cloud_firestore/lib/cloud_firestore.dart
@@ -1,3 +1,6 @@
 export 'package:brick_cloud_firestore/cloud_firestore_adapter.dart';
 export 'package:brick_cloud_firestore/cloud_firestore_model_dictionary.dart';
 export 'package:brick_cloud_firestore/cloud_firestore_provider.dart';
+
+export 'package:brick_cloud_firestore_abstract/cloud_firestore_model.dart';
+export 'package:brick_cloud_firestore_abstract/annotations.dart';
