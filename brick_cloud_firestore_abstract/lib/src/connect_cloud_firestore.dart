ee28e8da8f9a9fad9b77e855462ad00c882f2d50
John Huynh <john@huynh.io>
2020-02-03 07:54:44 -0800

    Update ConnectCloudFirestore annotation class to support class level config

--- brick_cloud_firestore_abstract/lib/src/connect_cloud_firestore.dart
+++ brick_cloud_firestore_abstract/lib/src/connect_cloud_firestore.dart
@@ -1,4 +1,10 @@
+import 'package:brick_cloud_firestore_abstract/src/cloud_firestore_serializable.dart';
+
 /// Class level annotation for just CloudFirestore.
 class ConnectCloudFirestore {
-  const ConnectCloudFirestore();
+  final CloudFirestoreSerializable cloudFirestoreConfig;
+
+  const ConnectCloudFirestore({
+    this.cloudFirestoreConfig,
+  });
 }
