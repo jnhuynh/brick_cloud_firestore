9ab348a9e49abd36b3bb73b7a41b0be13f401588
John Huynh <john@huynh.io>
2020-02-03 07:20:25 -0800

    Rename file for clarity

--- brick_cloud_firestore/example/lib/app/repository.dart
+++ brick_cloud_firestore/example/lib/app/repository.dart
@@ -1,5 +1,5 @@
 import 'package:brick_cloud_firestore/cloud_firestore.dart';
-import 'package:brick_cloud_firestore/cloud_firestore_repository.dart';
+import 'package:brick_cloud_firestore/offline_first_with_cloud_firestore_repository.dart';
 // run flutter pub run build_runner build before using this example
 import 'package:pizza_shoppe/app/db/schema.g.dart';
 import 'brick.g.dart';
--- brick_offline_first_with_cloud_firestore_build/lib/builder.dart
+++ brick_offline_first_with_cloud_firestore_build/lib/builder.dart
@@ -13,7 +13,7 @@ Builder cloudFirestoreAdaptersBuilder(options) {
 
 Builder offlineFirstAggregateBuilder(options) => AggregateBuilder(requiredImports: [
       "import 'package:brick_cloud_firestore_abstract/annotations.dart';",
-      "import 'package:brick_cloud_firestore/cloud_firestore_repository.dart';",
+      "import 'package:brick_cloud_firestore/offline_first_with_cloud_firestore_repository.dart';",
       "import 'package:brick_cloud_firestore/cloud_firestore_adapter.dart';",
       "import 'package:brick_cloud_firestore/cloud_firestore_provider.dart';",
       "import 'package:brick_offline_first/offline_first.dart';",
