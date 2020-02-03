aad4bb96ac4a77c83b112da0100302f4effb36cf
John Huynh <john@huynh.io>
2020-02-03 08:17:03 -0800

    Rename to cloud firestore repository and import it into generated files

--- brick_cloud_firestore/lib/cloud_firestore_repository.dart
+++ brick_cloud_firestore/lib/cloud_firestore_repository.dart
@@ -2,6 +2,6 @@ import 'package:brick_cloud_firestore/cloud_firestore.dart';
 import 'package:brick_cloud_firestore_abstract/cloud_firestore_model.dart';
 import 'package:brick_core/core.dart';
 
-class ConnectCloudFirestoreRepository extends SingleProviderRepository<CloudFirestoreModel> {
-  ConnectCloudFirestoreRepository() : super(CloudFirestoreProvider());
+class CloudFirestoreRepository extends SingleProviderRepository<CloudFirestoreModel> {
+  CloudFirestoreRepository() : super(CloudFirestoreProvider());
 }
--- brick_connect_cloud_firestore_build/lib/builder.dart
+++ brick_connect_cloud_firestore_build/lib/builder.dart
@@ -12,7 +12,7 @@ Builder cloudFirestoreAdaptersBuilder(options) {
 
 Builder connectCloudFirestoreAggregateBuilder(options) => AggregateBuilder(requiredImports: [
       "import 'package:brick_cloud_firestore_abstract/annotations.dart';",
-      "import 'package:brick_cloud_firestore/offline_first_with_cloud_firestore_repository.dart';",
+      "import 'package:brick_cloud_firestore/cloud_firestore_repository.dart';",
       "import 'package:brick_cloud_firestore/cloud_firestore_adapter.dart';",
       "import 'package:brick_cloud_firestore/cloud_firestore_provider.dart';",
     ]);
