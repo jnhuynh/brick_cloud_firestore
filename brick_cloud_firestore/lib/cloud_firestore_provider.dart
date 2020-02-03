cf513dd4f6fa62e78720004f3af78c1fec2b4a04
John Huynh <john@huynh.io>
2020-02-03 09:29:51 -0800

    Use setData instead of updateData for proper upsert

--- brick_cloud_firestore/lib/cloud_firestore_provider.dart
+++ brick_cloud_firestore/lib/cloud_firestore_provider.dart
@@ -58,7 +58,7 @@ class CloudFirestoreProvider implements brick.Provider<CloudFirestoreModel> {
     final collection = Firestore.instance.collection(adapter.collectionNodeKey);
     final document = collection.document();
     final data = await adapter.toCloudFirestore(instance, provider: this, repository: repository);
-    await document.updateData(data);
+    await document.setData(data);
 
     return instance..documentId = document.documentID;
   }
